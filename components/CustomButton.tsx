99f5366d90c5f68348601af4959d6bc624fd3be0
abdalla <abdallashaban584@gmail.com>
2024-01-09 19:01:49 +0200

    implement custombutton, custombutton prop types

---
+++ components/CustomButton.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { CustomButtonProps } from "@/types";
+
+const CustomButton = ({
+  title,
+  containerStyles,
+  btnType,
+  handleClick,
+}: CustomButtonProps) => {
+  return (
+    <button
+      type={btnType || "button"}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+      <span className="flex-1">{title}</span>
+    </button>
+  );
+};
+
+export default CustomButton;
