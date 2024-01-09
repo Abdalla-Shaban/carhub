a5ec5ae625a72b227f8786ec1730740b74c21b12
abdalla <abdallashaban584@gmail.com>
2024-01-09 19:39:01 +0200

    implement Hero section

---
+++ components/Hero.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import CustomButton from "./CustomButton";
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="flex-1 pt-20 padding-x">
+        <h1 className="hero__title">
+          Find, book, or rent a car - quickly and easily!
+        </h1>
+        <p className="hero__subtitle">
+          Streamline you car rental experince with our effortless booking
+          process
+        </p>
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue rounded-full text-white mt-10"
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image
+            src="/hero.png"
+            alt="hero image"
+            fill
+            className="object-contain"
+          />
+        </div>
+        <div className="hero__image-overlay" />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,6 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import CustomButton from "./CustomButton";
+import Hero from "./Hero";
 
-export { Header, Footer, CustomButton };
+export { Header, Footer, CustomButton, Hero };
