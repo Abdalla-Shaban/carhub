301656ce4c975b7320bbc43b4cb0fca38ebb3fa5
abdalla <abdallashaban584@gmail.com>
2024-01-14 00:03:04 +0200

    delete components index file

--- components/index.ts
+++
@@ -1,6 +0,0 @@
-import Header from "./Header";
-import Footer from "./Footer";
-import CustomButton from "./CustomButton";
-import Hero from "./Hero";
-
-export { Header, Footer, CustomButton, Hero };
